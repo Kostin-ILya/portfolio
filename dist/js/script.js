fb5f843a800db46bb8205bd72faa08564a0b3615
Ilya <lord-ilja@yandex.ru>
2022-02-09 05:38:23 +0300

    Script %ratings

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector('.hamburger'); 
-const menu = document.querySelector('.menu');
-const closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
@@ -8,4 +8,12 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
   menu.classList.remove('active');
+});
+
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line');
+
+counters.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML
 });
