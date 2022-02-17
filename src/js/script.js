e50c4755022ffe32d9c874c6a1d958142d25fdb6
Ilya <lord-ilja@yandex.ru>
2022-02-17 04:34:24 +0300

    Animate,wow JS, PhPMailer, Validate form , modal #thanks, change 320 FORM contacts

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,69 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
 	lines[i].style.width = item.innerHTML
 });
+
+$(document).ready(function(){
+  // $('[data-modal=appeal]').on('click', function(){
+  //   $('.overlay, #thanks').fadeIn('slow');
+  // });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #thanks').fadeOut('slow')
+  });
+  
+  function validateForms(form){
+    $(form).validate({
+      rules: {      
+        name: {
+          required: true,
+          minlength: 2
+        },        
+        email: {
+          required: true,
+          email: true
+        },
+        terms: {
+          required : true
+       }
+      },
+      messages: {
+        name: {     
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        },
+        terms: {
+          required : "check the checbox"
+      }
+      }
+    });
+  };
+
+  validateForms('#appeal-form');
+
+
+$('form').submit(function(event){
+  event.preventDefault();
+  if (!$(this).valid()) {
+    return;
+  }
+  $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+  }).done(function(){
+      $(this).find("input").val("");
+      $(this).find("textarea").val("");
+      
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+  });
+  return false;
+});
+
+new WOW().init();
+
+  
+});
